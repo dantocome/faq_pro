9dcf231512379bfc72f015e026d707d677be0c03
Daniel Egele <danielegele9@gmail.com>
2023-12-18 15:10:18 +0100

    all added files

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
-  const para1 = document.querySelector(".para1")  ;
+  const para1 = document.querySelector(".para1");
+  const para2 = document.querySelector(".para2")
 
   para1.addEventListener("click", function(){
     const answer1 = this.nextElementSibling;
@@ -14,4 +15,16 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
   });
+  
+  para2 .addEventListener("click", function(){
+    const answer2 = this.nextElementSibling;
+    const plus2 = document.getElementById("plus2");
+    if(answer2.style.display==="block"){
+        answer2.style.display = "none";
+        plus2.textContent = "-"
+    }else{
+        answer2.style.display = "block";
+        plus2.textContent = "+"
+    }
+  })
 });
